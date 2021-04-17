f78bdd5b1aae63fc1456dd6a17bd68147b9bdf79
TomPenguin <tom.penguin.zoo@gmail.com>
2021-04-17 13:34:59 +0900

    add jest

---
+++ src/compositions/__tests__/useMessages.test.ts
@@ -0,0 +1,5 @@
+describe("test", () => {
+  it("test", () => {
+    expect(0).toBe(0);
+  });
+});
---
+++ src/compositions/useMessages.ts
@@ -0,0 +1,3 @@
+import { Browser } from "webextension-polyfill-ts";
+
+export const getMessages = () => {};
