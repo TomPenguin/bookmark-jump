3af6720028049545f288a4dae57422e044a92f79
TomPenguin <tom.penguin.zoo@gmail.com>
2021-04-17 13:00:20 +0900

    remove unused files

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,3 @@
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Hello from the background", request, sender, sendResponse);
 });
-
-chrome.commands.onCommand.addListener((c) => console.log(c));
